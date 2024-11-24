c5e623e01ee86776aa668df20b9aa06d8419438f
Ilya Caramishev <ilya@qrdl.com>
2024-11-24 11:47:50 +0200

    Fix preprod directive name

--- mem_darwin.h
+++ mem_darwin.h
@@ -3,7 +3,7 @@
 // This work is licensed under the terms of the Apache License, Version 2.0
 // For a copy, see <https://opensource.org/license/apache-2-0>.
 
-#ifdef __APPLE_
+#ifdef __APPLE__
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
