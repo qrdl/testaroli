0ea342f2bde1fab32c571e15b6f5391b0dc53a56
Ilya Caramishev <ilya@qrdl.com>
2024-12-01 11:48:53 +0200

    Added more unit tests

--- reset_test.go
+++ reset_test.go
@@ -102,5 +102,87 @@ func TestInvalidReset(t *testing.T) {
 		}
 	}()
 
-	Reset(1) // not functions
+	Reset(1) // not a functions
+}
+
+func TestResetAllOne(t *testing.T) {
+	Override(TestingContext(t), qux, Once, func(err error) error {
+		Expectation()
+		return nil
+	})
+
+	ResetAll(qux)
+
+	err := bar(3)
+	if err.Error() != "odd" {
+		t.Errorf("unexpected")
+	}
+	testError(t, nil, ExpectationsWereMet())
+}
+
+func TestResetAllSeveral(t *testing.T) {
+	Override(TestingContext(t), qux, Once, func(err error) error {
+		Expectation()
+		return errors.New("first")
+	})
+
+	Override(TestingContext(t), baz, Once, func(i int) error {
+		Expectation()
+		return errors.New("baz")
+	})
+
+	Override(TestingContext(t), qux, Once, func(err error) error {
+		Expectation()
+		return errors.New("second")
+	})
+
+	ResetAll(qux)
+
+	err := bar(3) // should call original
+	if err.Error() != "odd" {
+		t.Errorf("unexpected")
+	}
+
+	err = baz(1) // should call mock
+	if err.Error() != "baz" {
+		t.Errorf("unexpected")
+	}
+
+	testError(t, nil, ExpectationsWereMet())
+}
+
+func TestInvalidResetAll(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	ResetAll(1) // not a functions
+}
+
+func TestResetAllAlways(t *testing.T) {
+	Override(TestingContext(t), qux, Always, func(err error) error {
+		Expectation()
+		return errors.New("first")
+	})
+
+	Override(TestingContext(t), baz, Once, func(i int) error {
+		Expectation()
+		return errors.New("baz")
+	})
+
+	ResetAll(qux)
+
+	err := bar(3) // should call original
+	if err.Error() != "odd" {
+		t.Errorf("unexpected")
+	}
+
+	err = baz(1) // should call mock
+	if err.Error() != "baz" {
+		t.Errorf("unexpected")
+	}
+
+	testError(t, nil, ExpectationsWereMet())
 }
