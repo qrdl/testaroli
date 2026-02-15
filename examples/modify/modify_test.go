4d2db42e9461f6b59f4c3f2216c9228dd6621ea6
Ilya Caramishev <ilya@qrdl.com>
2026-02-15 10:55:16 +0200

    Refactor examples

--- examples/modify/modify_test.go
+++ examples/modify/modify_test.go
@@ -10,22 +10,6 @@ import (
 	. "github.com/qrdl/testaroli"
 )
 
-func genRandom() int {
-	timestamp := time.Now().Unix()
-	src := rand.NewSource(timestamp)
-	r := rand.New(src)
-	return r.Intn(100)
-}
-
-func openFile(filename string) (*os.File, error) {
-	fs, err := os.Open(filename)
-	if err == os.ErrPermission {
-		return nil, os.ErrNotExist // hide file existance if user has no permissions
-	}
-
-	return fs, nil
-}
-
 func TestReplaceArg(t *testing.T) {
 	Override(TestingContext(t), rand.NewSource, Once, func(seed int64) rand.Source {
 		Expectation()
